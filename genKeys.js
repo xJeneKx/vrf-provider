fa3d8fb2563cf57a14798fe1a4b5ea1c1ae8f4a9
Evgenii Stulnikov <xxjenekxx@gmail.com>
2024-04-09 03:15:40 +0300

    fixed gen keys, now work on all platforms

--- genKeys.js
+++ genKeys.js
@@ -1,35 +1,32 @@
 const fs = require('node:fs');
-const { spawnSync } = require('node:child_process');
 const process = require('node:process');
-const { join } = require("path");
+const { generateKeyPairSync } = require('node:crypto');
+const { join } = require('path');
 const pathToFolder = join(__dirname, 'keys');
+const pathToPrivKey = join(pathToFolder, 'priv_key.pem');
+const pathToPubKey = join(pathToFolder, 'pub_key.pem');
 
 if (!fs.existsSync(pathToFolder)) {
 	fs.mkdirSync(pathToFolder);
 }
 
-if (fs.existsSync(join(pathToFolder, 'priv_key.pem')) ||
-	fs.existsSync(join(pathToFolder, 'pub_key.pem'))) {
+if (fs.existsSync(pathToPrivKey) || fs.existsSync(pathToPubKey)) {
 	console.error('keys already exists');
 	process.exit(1);
 }
 
-// openssl genrsa -out priv_key.pem 2048
-const args = ['genrsa', '-out', join(pathToFolder, 'priv_key.pem'), '2048'];
-const result = spawnSync('openssl', args);
-if (result.error) {
-	console.error(result.error);
-	process.exit(1);
-}
+const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+	modulusLength: 2048,
+});
 
-// openssl rsa -in priv_key.pem -outform PEM -pubout -out pub_key.pem
-const args2 = ['rsa', '-in', join(pathToFolder, 'priv_key.pem'), '-outform', 'PEM', '-pubout', '-out', join(pathToFolder, 'pub_key.pem')];
-const result2 = spawnSync('openssl', args2);
-if (result2.error) {
-	console.error(result2.error);
-	process.exit(1);
-}
+fs.writeFileSync(pathToPrivKey, privateKey.export({
+	type: 'pkcs1',
+	format: 'pem',
+}));
+
+fs.writeFileSync(pathToPubKey, publicKey.export({
+	type: 'pkcs1',
+	format: 'pem',
+}));
 
 console.log('done');
-process.exit(0);
-
